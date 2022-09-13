78c9462af331c710cdc9a354d2dd7882acaace6d
soccemon <78286971+soccemon@users.noreply.github.com>
2022-09-13 10:24:03 -0400

    port updates

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
+const port = process.env.PORT || 3000
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
@@ -95,6 +96,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is running on 3000')
+app.listen(port, () => {
+    console.log('Server is running on ' + port)
 })
